9645e1df86703c81350767ee0b770b0ecde3e96c
Dextron11 <gltron9@gmail.com>
2017-12-07 23:33:41 +0300

    Update main.cpp

--- kurs/main.cpp
+++ kurs/main.cpp
@@ -1,23 +1,11 @@
-#include <iostream>
-#include "Virt.h"
-#include "Spisok.h"
+#include "Graf.h"
 #include "printGraf.h"
 
 
 int main() {
     Spisok A;
-    Lref t; //Рабочий указатель для перемещения
-    //по списку заголовочных звеньев.
-    //Построение графа и вывод его структуры Вирта.
     A.MakeGraph();
-
-    t = A.GetHead();
-    while (t != A.GetTail()) {
-        (*t).Flag = true;
-        t = (*t).Next;
-    }
     A.Ostov_Breadth();
-    std::cout << "\n";
     printG();
 
 }
